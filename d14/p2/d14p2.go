a80958a22a816920cd4d609421efd8d9e5a18f2f
Carlos Castro <carlos.castro@mongodb.com>
2022-12-19 14:26:13 +0000

    d14 complete;

--- d14/p2/d14p2.go
+++ d14/p2/d14p2.go
@@ -2,11 +2,88 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
-	"log"
 	"os"
+	"strconv"
+	"strings"
 )
+
+const MaxUint = ^uint(0)
+const MaxInt = int(MaxUint >> 1)
+
+type Edges struct {
+	minX, maxX, minY, maxY int
+}
+
+type Point struct {
+	y, x int
+}
+
+type Line struct {
+	points []Point
+}
+
+type Lines []Line
+
+type Cavern struct {
+	m      map[Point]string
+	edges  Edges
+	source Point
+	sand   []Point
+}
+
+func (l *Lines) GetEdges() Edges {
+	var minX, maxX, minY, maxY int
+	minY = 0
+	minX = MaxInt
+	for _, v := range *l {
+		for _, v := range v.points {
+
+			if v.x < minX {
+				minX = v.x
+			}
+			if v.x > maxX {
+				maxX = v.x
+			}
+			if v.y > maxY {
+				maxY = v.y
+			}
+		}
+	}
+
+	return Edges{minX: minX, minY: minY, maxX: maxX, maxY: maxY}
+}
+
+func (l *Lines) GetPointType(p Point) string {
+
+	for _, v := range *l {
+
+		for i := 0; i < len(v.points)-1; i++ {
+			if p.x >= v.points[i].x && p.x <= v.points[i+1].x && p.y >= v.points[i].y && p.y <= v.points[i+1].y {
+				return "#"
+			}
+
+			if p.x >= v.points[i+1].x && p.x <= v.points[i].x && p.y >= v.points[i+1].y && p.y <= v.points[i].y {
+				return "#"
+			}
+		}
+	}
+
+	return "."
+}
+
+func (l *Lines) Print() {
+	edges := l.GetEdges()
+
+	for y := edges.minY; y <= edges.maxY; y++ {
+		for x := edges.minX; x <= edges.maxX; x++ {
+
+			pointType := l.GetPointType(Point{x: x, y: y})
+			fmt.Printf(pointType)
+		}
+		fmt.Printf("\n")
+	}
+}
 
 func readFile(fileName string) ([]string, error) {
 	output := make([]string, 0)
@@ -25,75 +102,148 @@ func readFile(fileName string) ([]string, error) {
 	return output, nil
 }
 
-func compareLists(leftList any, rightList any) int {
+func initMap(m map[Point]string, l *Lines, edges Edges) {
 
-	leftArray, leftIsArray := leftList.([]any)
-	rightArray, rightIsArray := rightList.([]any)
+	for y := edges.minY; y <= edges.maxY; y++ {
+		for x := edges.minX; x <= edges.maxX; x++ {
+			point := Point{x: x, y: y}
+			pointType := l.GetPointType(point)
 
-	// both are integers
-	if !leftIsArray && !rightIsArray {
-		return int(leftList.(float64) - rightList.(float64))
-	} else if !leftIsArray {
-		// left is integer and right is list
-		leftArray = []any{leftList}
-	} else if !rightIsArray {
-		// left is list and right is integer
-		rightArray = []any{rightList}
-	}
+			if y == edges.maxY {
+				m[point] = "#"
+			} else {
+				m[point] = pointType
+			}
 
-	// both values are lists
-	for i := 0; i < len(leftArray) && i < len(rightArray); i++ {
-		// compare each element
-		// if they are the same keep going, otherwise return
-		if result := compareLists(leftArray[i], rightArray[i]); result != 0 {
-			return result
 		}
+
 	}
+}
 
-	return len(leftArray) - len(rightArray)
+func (c *Cavern) PrintMap() {
+	fmt.Printf("    ")
+	for x := c.edges.minX; x <= c.edges.maxX; x++ {
+		fmt.Printf("%v ", x)
+	}
+	fmt.Printf("\n")
 
+	for y := c.edges.minY; y <= c.edges.maxY; y++ {
+		fmt.Printf(" %v ", y)
+		for x := c.edges.minX; x <= c.edges.maxX; x++ {
+			point := Point{x: x, y: y}
+			fmt.Printf("  %v ", c.m[point])
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func (c *Cavern) MoveSand(sand Point) Point {
+	moved := true
+	restPos := sand
+
+	for y := sand.y; y <= c.edges.maxY && moved; y++ {
+		moved = false
+		originalPos := sand
+		// down
+		sand.y++
+
+		switch c.m[sand] {
+		case "#":
+		case "o":
+		case ".":
+			c.m[sand] = "o"
+			restPos = sand
+			c.m[originalPos] = "."
+			moved = true
+		}
+
+		if !moved {
+			// down and left
+			sand.x--
+
+			switch c.m[sand] {
+			case "#":
+			case "o":
+			case ".":
+				c.m[sand] = "o"
+				restPos = sand
+				c.m[originalPos] = "."
+				moved = true
+			}
+
+			if !moved {
+				// down and right
+				sand.x += 2
+				switch c.m[sand] {
+				case "#":
+				case "o":
+				case ".":
+					c.m[sand] = "o"
+					restPos = sand
+					c.m[originalPos] = "."
+					moved = true
+				}
+			}
+		}
+
+	}
+	return restPos
+}
+
+func (c *Cavern) AddSand() bool {
+	sand := c.source
+	c.m[sand] = "o"
+
+	restPos := c.MoveSand(sand)
+	if restPos == c.source {
+		fmt.Println("reached the source", len(c.sand)+1)
+		return false
+	}
+	c.sand = append(c.sand, restPos)
+	//c.PrintMap()
+
+	return true
 }
 
 func main() {
-	puzzle, _ := readFile("d13/puzzle_input.txt")
+	puzzle, _ := readFile("d14/puzzle_input.txt")
 
-	var div1 any
-	var div2 any
-
-	json.Unmarshal([]byte("[[2]]"), &div1)
-	json.Unmarshal([]byte("[[6]]"), &div2)
-
-	div1Index := 1
-	div2Index := 2
-
-	for i := 0; i < len(puzzle)-1; i += 3 {
-		left := puzzle[i]
-		right := puzzle[i+1]
-
-		var leftList any
-		var rightList any
-
-		if err := json.Unmarshal([]byte(left), &leftList); err != nil {
-			log.Fatal(err)
-		}
-		if err := json.Unmarshal([]byte(right), &rightList); err != nil {
-			log.Fatal(err)
-		}
-
-		if compareLists(leftList, div1) <= 0 {
-			div1Index++
-		}
-		if compareLists(leftList, div2) <= 0 {
-			div2Index++
-		}
-		if compareLists(rightList, div1) <= 0 {
-			div1Index++
-		}
-		if compareLists(rightList, div2) <= 0 {
-			div2Index++
-		}
-
+	lines := Lines{}
+	source := Point{
+		x: 500,
+		y: 0,
 	}
 
-	fmt.Println(div1Index * div2Index)
+	m := make(map[Point]string, 0)
+
+	for _, v := range puzzle {
+		points := strings.Split(v, " -> ")
+		line := Line{}
+		for _, point := range points {
+			data := strings.Split(point, ",")
+			x, _ := strconv.Atoi(data[0])
+			y, _ := strconv.Atoi(data[1])
+
+			line.points = append(line.points, Point{x: x, y: y})
+		}
+		lines = append(lines, line)
+	}
+
+	edges := lines.GetEdges()
+	edges.maxY += 2
+	edges.minX -= 1000
+	edges.maxX += 1000
+	initMap(m, &lines, edges)
+	m[source] = "+"
+
+	cavern := Cavern{
+		m:      m,
+		edges:  edges,
+		source: source,
+	}
+
+	cont := true
+	for ok := true; ok; ok = cont {
+		cont = cavern.AddSand()
+	}
 }
