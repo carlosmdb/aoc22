92fe899302c2ed1ceda307ea59bacd946111a7bd
Carlos Castro <carlos.castro@mongodb.com>
2022-12-20 14:39:25 +0000

    d18p1 complete

--- d18/p1/d18p1.go
+++ d18/p1/d18p1.go
@@ -25,16 +25,32 @@ func readFile(fileName string) ([]string, error) {
 	return output, nil
 }
 
+type CubeSize struct {
+	x, y, z int
+}
+
+type CubeFaces struct {
+	xy1, xy2, yz1, yz2, xz1, xz2 bool
+}
+
 type Cube struct {
 	x, y, z int
+	conn    CubeFaces
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
 
 func main() {
 	puzzle, _ := readFile("d18/puzzle_input.txt")
 
+	size := CubeSize{1, 1, 1}
 	cubes := make([]Cube, 0)
 	for _, v := range puzzle {
-		fmt.Println(v)
 		parts := strings.Split(v, ",")
 		x, _ := strconv.Atoi(parts[0])
 		y, _ := strconv.Atoi(parts[1])
@@ -44,6 +60,60 @@ func main() {
 		cubes = append(cubes, c)
 	}
 
-	fmt.Println(cubes)
+	for i := 0; i < len(cubes); i++ {
+		for j := 0; j < len(cubes); j++ {
+			if cubes[i] != cubes[j] {
+				//compare x and y - only z moves
+				if cubes[i].x == cubes[j].x && cubes[i].y == cubes[j].y && cubes[i].z == cubes[j].z+size.z {
+					cubes[i].conn.xy1 = true
+				}
+				if cubes[i].x == cubes[j].x && cubes[i].y == cubes[j].y && cubes[i].z+size.z == cubes[j].z {
+					cubes[i].conn.xy2 = true
+				}
+
+				//compare x and z - only y moves
+				if cubes[i].x == cubes[j].x && cubes[i].z == cubes[j].z && cubes[i].y == cubes[j].y+size.y {
+					cubes[i].conn.xz1 = true
+				}
+				if cubes[i].x == cubes[j].x && cubes[i].z == cubes[j].z && cubes[i].y+size.y == cubes[j].y {
+					cubes[i].conn.xz2 = true
+				}
+
+				//compare y and z - only x moves
+				if cubes[i].y == cubes[j].y && cubes[i].z == cubes[j].z && cubes[i].x == cubes[j].x+size.x {
+					cubes[i].conn.yz1 = true
+				}
+				if cubes[i].y == cubes[j].y && cubes[i].z == cubes[j].z && cubes[i].x+size.x == cubes[j].x {
+					cubes[i].conn.yz2 = true
+				}
+
+			}
+		}
+	}
+
+	//fmt.Println(cubes)
+	notConnected := 0
+	for _, v := range cubes {
+		if !v.conn.xy1 {
+			notConnected++
+		}
+		if !v.conn.xy2 {
+			notConnected++
+		}
+		if !v.conn.yz1 {
+			notConnected++
+		}
+		if !v.conn.yz2 {
+			notConnected++
+		}
+		if !v.conn.xz1 {
+			notConnected++
+		}
+		if !v.conn.xz2 {
+			notConnected++
+		}
+	}
+
+	fmt.Println(notConnected)
 
 }
